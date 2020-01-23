0ce8ea7d2511a24384f158648255b124e9a2b057
Kurt Krafft <kkrafft6609@gmail.com>
2020-01-23 09:19:39 -0600

    challenges

--- main.js
+++ main.js
@@ -2,10 +2,15 @@ const storeThings = () => {
     const article = document.getElementById('putEmHere');
     const userThing = document.getElementById('favThing');
     const userPlace = document.getElementById('buyThing');
+    if (userThing.value == "" || userPlace.value == "") {
+        alert('Please fill out all the fields')
+    }else {
     const html = `<div class="userInput"><h5>I can get ${userThing.value} at ${userPlace.value}.</h5></div>`
     article.innerHTML += html;
     userThing.value = "";
     userPlace.value="";
+    userThing.focus();
+    }
 }
 const submitButton = document.getElementById('submitBtn');
 submitButton.addEventListener('click', storeThings);
